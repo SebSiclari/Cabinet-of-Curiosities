7ef2d5c566564ba3368a3174beadba6033af98b8
OliverG <gallantioliver@gmail.com>
2022-09-30 20:58:04 +0200

    Added some ts files

---
+++ client/firebase.ts
@@ -0,0 +1,21 @@
+import * as Firebase from 'firebase';
+
+// Your web app’s Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCeKN38eT2LZgdZOq4MQ6UHjN_EGLUTV0s',
+  authDomain: 'cabinet-of-curiosities-eec59.firebaseapp.com',
+  projectId: 'cabinet-of-curiosities-eec59',
+  storageBucket: 'cabinet-of-curiosities-eec59.appspot.com',
+  messagingSenderId: '256047849920',
+  appId: '1:256047849920:web:c0b3394ca763e8c0772825',
+};
+let app;
+if (Firebase.apps.length === 0) {
+  app = Firebase.initializeApp(firebaseConfig);
+} else {
+  app = Firebase.app();
+}
+const auth = Firebase.auth();
+const db = Firebase.firestore();
+
+export { auth, db, firebaseConfig };
--- client/src/services/exhibitionService.ts
+++ client/src/services/exhibitionService.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { BASE_URL, API_KEY } from '@env';
+//import { BASE_URL, API_KEY } from '@env';
+const BASE_URL = 'https://api.harvardartmuseums.org/exhibition';
+const API_KEY = 'e08d59a9-df44-4fea-a4fb-34e9addc0de2';
 
 export async function getExhibitions() {
   try {
